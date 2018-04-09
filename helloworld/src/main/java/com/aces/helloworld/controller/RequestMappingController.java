072147529cba177030915b3179cff6ce46030ab2
lxy <acesdream@126.com>
2018-04-09 12:04:44 +0800

    @RequestMapping的params和headers演示

--- helloworld/src/main/java/com/aces/helloworld/controller/RequestMappingController.java
+++ helloworld/src/main/java/com/aces/helloworld/controller/RequestMappingController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/springmvc")
@@ -23,6 +24,37 @@ public class RequestMappingController {
     @RequestMapping("/hello")
     public String helloWorld() {
         logger.info("RequestMapping可以修饰方法，也可以修饰类");
+        return "success";
+    }
+
+    /**
+     * 1、@RequestMapping 的属性，
+     * value：定义映射的URL
+     * method：定义请求的方式（GET、POST、PUT、DELETE）
+     *
+     * params：请求参数
+     * headers：请求头
+     * 其中params和heads支持简单的表达式
+     * param1：表示请求必须包含参数名为param1的参数
+     * !param1：表示请求参数不能包含名为param1的参数
+     * param1 != value1：表示请求包含参数名为param1的参数，但是其值不能是value1
+     * {"param1=value1", "param2"}：表示请求参数包含名为param1和param2的两个参数，且param1的参数值为value1
+     *
+     * @return 成功页面
+     */
+    @RequestMapping(value = "/method", method = RequestMethod.POST)
+    public String requestMappingMethod() {
+        logger.info("RequestMapping属性使用说明");
+        return "success";
+    }
+
+    /**
+     * params和headers可以更加精确的映射请，不过使用到的不多，最常用的还是method
+     * @return
+     */
+    @RequestMapping(value = "/paramsAndHeads", params = {"usernmae", "age=11"}, headers = {"Accept-Language=en-US"})
+    public String requestMappingParamsAndHeads() {
+        logger.info("RequestMapping属性使用说明");
         return "success";
     }
 }
